89374ed87faa469f7a8e36ca7c3249c2b33353e9
swifuCS <xlrgameryt04d@gmail.com>
2021-02-04 09:26:22 +0100

    Fiksa alt, dette er den beste versjonen så langt  04.02.2021

--- Min versjon av dette/Stiler/rootCode.js
+++ Min versjon av dette/Stiler/rootCode.js
@@ -11,13 +11,33 @@ let numberValue = [Math.floor(Math.random() *100)]
     if (input == numberValue){
     answer.innerHTML = `Congratulations, you guessed the right number that was ${numberValue}`
     document.getElementById (`outputAlert`).style.color=("purple")
+    document.getElementById (`right`).play();
 }else if (input < numberValue){
-    answer.innerHTML = "Too low!"
+    answer.innerHTML = "Too low!" 
     document.getElementById (`outputAlert`).style.color=("Blue")
+    document.getElementById (`wrong`).play();
 }
 if (input > numberValue){
     answer.innerHTML = "Too high!"
     document.getElementById (`outputAlert`).style.color=("red")
-};
+    document.getElementById (`wrong`).play();
+   };
+if (isNaN(input)){
+    answer.innerHTML = "Please use numbers"
+    document.getElementById (`outputAlert`).style.color=("green")
+    document.getElementById (`win-sound`).play();
+}
 })
 
+ 
+
+let playIcon = document.getElementById ("playIcon");
+let sang = document.getElementById ("sang");
+
+playIcon.onclick = function(){
+    if(document.getElementById(`sang`).paused){
+        document.getElementById(`sang`).play();
+    }else{
+        document.getElementById(`sang`).pause();
+    }
+};
